2ecd461f8dd916b14d15571e0dea62730aa2aafe
euonymus <euonymus0220@gmail.com>
2018-10-17 17:59:35 +0900

    drawer try

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,12 +3,12 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import MenuScreen from '../screens/menu-screen';
+import ThreeDrawer from './three-drawer';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
-  Home: MenuScreen,
+  Home: ThreeDrawer,
 });
 
 HomeStack.navigationOptions = {
---
+++ navigation/three-drawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+
+// import { Button, Text, Platform, ScrollView, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from 'react-navigation';
+
+import FirstScreen from '../screens/first-screen';
+import SecondScreen from '../screens/second-screen';
+const ThreeDrawer = createDrawerNavigator(
+    {
+	First: {
+	    path: '/',
+	    screen: FirstScreen,
+	},
+	Second: {
+	    path: '/sent',
+	    screen: SecondScreen,
+	},
+    },
+    {
+	initialRouteName: 'First',
+	drawerPosition: 'left',
+	contentOptions: {
+	    activeTintColor: 'red',
+	}
+    }
+);
+export default ThreeDrawer;
